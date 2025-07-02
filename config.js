ccddba2a36df6765d90aea21369f81c492c1b79f
kea-nelly <kealeboga.serapane24@gmail.com>
2025-07-02 12:03:57 +0200

    Adding Home link function

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
 function checkApiKey() {
-  if (localStorage.getItem("apiKey")) {
+  if (!localStorage.getItem("apiKey")) {
     window.open("enter-api-key.html", "_self");
   }
   return localStorage.getItem("apiKey");
